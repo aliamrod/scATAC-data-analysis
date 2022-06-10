8ea3d1d659a7053d756c0c645aaa60fb2ec556ef
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2022-06-09 21:15:11 -0500

    Update 1_scATAC_analysis.r

--- 1_scATAC_analysis.r
+++ 1_scATAC_analysis.r
@@ -4,6 +4,10 @@
 # $ module load hdf5_18/1.8.17
 # $ module load gcc/8.3.0
 # $ module load gsl/2.4
+# $ R
+
+# setwd(dir); specify a working directory. This may be from the root directory and transitioning up to current directory.
+setwd('/work/Neuroinformatics_Core/s204365/ATACSeq_0001')
 
 # scATAC data analysis pipeline (integrates ArchR, v1.0.1)
 if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
@@ -12,58 +16,42 @@ if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocMana
 devtools::install_github("GreenleafLab/ArchR", ref="master", repos = BiocManager::repositories())
 library(ArchR) # version
 ArchR::installExtraPackages()
-set.seed(10)
-addArchGenome("hg38")
+set.seed(1)
+addArchGenome("mm10") # Mus musculus (house mouse) genome assembly
 
 # Input data from 10X Cellranger-ATAC output
-"scA/fragments.tsv.gz", "scB/fragments.tsv.gz", "scC/fragments.tsv.gz", "scD/fragments.tsv.gz", "scE/fragments.tsv.gz", "scF/fragments.tsv.gz", "scG/fragments.tsv.gz", "scH/fragments.tsv.gz", "scI/fragments.tsv.gz", "scJ/fragments.tsv.gz", "scK/fragments.tsv.gz", "scL/fragments.tsv.gz", "scM/fragments.tsv.gz", "scN/fragments.tsv.gz", "scO/fragments.tsv.gz",  "scP/fragments.tsv.gz", "scQ/fragments.tsv.gz", "scR/fragments.tsv.gz", "scS/fragments.tsv.gz", "scT/fragments.tsv.gz", "scU/fragments.tsv.gz", "scV/fragments.tsv.gz", "scW/fragments.tsv.gz", "scX/fragments.tsv.gz", "scY/fragments.tsv.gz", "scZ/fragments.tsv.gz", "scAA/fragments.tsv.gz", "scAB/fragments.tsv.gz", "scAC/fragments.tsv.gz", "scAD/fragments.tsv.gz", "scAE/fragments.tsv.gz", "scAF/fragments.tsv.gz", "scG/fragments.tsv.gz", "scAH/fragments.tsv.gz", "scAI/fragments.tsv.gz","scAJ/fragments.tsv.gz","scAK/fragments.tsv.gz","scAL/fragments.tsv.gz","scAM/fragments.tsv.gz","scAN/fragments.tsv.gz","scAO/fragments.tsv.gz","scAP/fragments.tsv.gz","scAQ/fragments.tsv.gz","scAR/fragments.tsv.gz")
-names(inputFiles) <- c("scA", "scB", "scC", "scD", "scE", "scF", "scG", "scH", "scI", "scJ", "scK", "scL", "scM", "scN", "scO", "scP", "scQ", "scR", "scS", "scT", "scU", "scV", "scW", "scX", "scY", "scZ", "scAA", "scAB", "scAC", "scAD", "scAE", "scAF", "scAG", "scAH", "scAI", "scAJ", "scAK", "scAL", "scAM", "scAN", "scAO", "scAP", "scAQ", "scAR")
+# list fragment files:
+fragment_tsv = list.files(pattern = ".tsv")
+inputFiles <- c(
+  "fragments_104.tsv", "fragments_105.tsv", "fragments_106.tsv", "fragments_107.tsv")
+names(inputFiles) <- c("fragments_104", "fragments_105", "fragments_106", "fragments_107")
 
-# create ArchR object
+# Create ArchR object.
 ArrowFiles <- createArrowFiles(
   inputFiles = inputFiles,
   sampleNames = names(inputFiles),
-  filterTSS = 4, 
-  filterFrags = 1000,
-  addTileMat = TRUE, 
-  force = FALSE, 
+  filterTSS = 4, # Do not set this too high due to notion that it can be adjusted to accordingly later.
+  filterFrags = 1000, 
+  addTileMat = TRUE,
+  force = FALSE,
   addGeneScoreMat = TRUE
   )
+projNEU1 <- ArchRProject(
+  ArrowFiles = ArrowFiles,
+  outputDirectory = "/work/Neuroinformatics_Core/s204365/ATACSeq_0001/output_dir", 
+  copyArrows = TRUE # Boolean value indicating whether ArrowFiles should be copied into ```outputDirectory```. This is recommended so that if you have to modify the ArrowFiles, you have an original copy for later usage. 
+  )
+
+  
+
+
+
+ 
 # Strict quality control (QC) of scATAC-seq data is essential to remove the contribution of low-quality cells. In ArchR, we will consider 3 characterstics of data:
 # 1. The number of unique nuclear fragments (i.e. not mapping to mitochondrial DNA)
 # 2. The signal-to-background ratio. Low signal-to-background ratio is often attributed to dead or dying cells which have de-chromatinzed DNA which allows for random transposition genome-wide.
 # 3. The fragment size distribution. Due to nucleosomal periodicity, we expect to see depletion of fragments that are the length of DNA wrapped around a nucleosome (approximately 147 bp). 
 
-projCAD1 <- ArchRProject(
-  ArrowFiles = ArrowFiles,
-  outputDirectory = "CAD", 
-  copyArrows = TRUE #Recommended so that if you modify Arrow files, you'll have an original copy for later usage.
-  )
-
-# add doublet score
-doubScores <- addDoubletScores(
-  input = ArrowFiles,
-  k = 10, #refers to how many cells near a "pseudo-doublet" to count
-  knnMethod = "UMAP", # refers to the embedding to use for nearest neighbor search
-  LSIMethod = 1, 
-  force = TRUe
-  )
-# basic QC
-proj_CAD_1 <- projCAD1
-p <- ggpoint(
-  x = df[,1],
-  y = df[,2],
-  colorDensity = TRUE,
-  continuousSet = "sambaNight", 
-  xlabel = "Log10 Unique Fragments", 
-  ylabel = "TSS Enrichment", 
-  xlim = c(log10(500), quantile(df[,1], probs = 0.99)),
-  ylim = c(0, quantile(df[,2], probs = 0.99))
-  ) + geom_hline(yintercept = 4, lty = "dashed") + geom_vlin(xintercept = 3, lty = "dashed")
-plotPDF(p, name = "TSS-vs-Frags.pdf", ArchRProf = proj_CAD_1, addDOC = FALSE)
-  
-  
-  //////////////////////////
 
 
   
