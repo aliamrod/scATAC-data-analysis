e53f119293e8c5fa26aad28eab6b4d06c7cd1e99
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2022-06-15 13:05:09 -0500

    Create scATAC-seq preprocessing.sh

---
+++ scATAC-seq preprocessing.sh
@@ -0,0 +1,7 @@
+$ cd /work/Neuroinformatics_Core/s204365/ATACSeq_0001/metadata
+# cut -d " " -f 2- fragments_104.tsv.gz > fragments104_processed.tsv.gz
+# cut -d " " -f 2- fragments_105.tsv.gz > fragments105_processed.tsv.gz
+# cut -d " " -f 2- fragments_106.tsv.gz > fragments106_processed.tsv.gz
+# cut -d " " -f 2- fragments_107.tsv.gz > fragments107_processed.tsv.gz
+
+
