4254eea48079bb556d95bffd946e5dd26801623e
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2022-06-16 12:03:31 -0500

    Update scATAC-seq preprocessing.sh

--- scATAC-seq preprocessing.sh
+++ scATAC-seq preprocessing.sh
@@ -1,10 +1,9 @@
 $ cd /work/Neuroinformatics_Core/s204365/ATACSeq_0001/metadata
 $ module load dos2unix/gcc/7.3.4
-$ dos2unix fragments_104.tsv.gz
+$ dos2unix fragments_104.tsv.gz fragments_105.tsv.gz fragments_106.tsv.gz fragments_107.tsv.gz
 
 $ cut -d " " -f 2- fragments_104.tsv.gz > fragments104_processed.tsv.gz
 $ cut -d " " -f 2- fragments_105.tsv.gz > fragments105_processed.tsv.gz
 $ cut -d " " -f 2- fragments_106.tsv.gz > fragments106_processed.tsv.gz
 $ cut -d " " -f 2- fragments_107.tsv.gz > fragments107_processed.tsv.gz
 
-# 
