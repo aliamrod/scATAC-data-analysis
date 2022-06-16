8784985f063a41b9c871b20fd4719aad5b3ca4bb
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2022-06-16 12:04:37 -0500

    Update scATAC-seq preprocessing.sh

--- scATAC-seq preprocessing.sh
+++ scATAC-seq preprocessing.sh
@@ -1,5 +1,8 @@
 $ cd /work/Neuroinformatics_Core/s204365/ATACSeq_0001/metadata
 $ module load dos2unix/gcc/7.3.4
+
+# Data inconsistency (if utilizing Windows OS) caused by preparing the data in Windows and passing it onto Linux. There is often an "M" character that is falsely recognized in Linux. 
+# Remove this special character from data file(s) by:
 $ dos2unix fragments_104.tsv.gz fragments_105.tsv.gz fragments_106.tsv.gz fragments_107.tsv.gz
 
 $ cut -d " " -f 2- fragments_104.tsv.gz > fragments104_processed.tsv.gz
