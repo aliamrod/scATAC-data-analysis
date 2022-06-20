bb30f23a714c8135646bbac30315646de5013107
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2022-06-20 15:59:30 -0500

    Update 01_scATAC-seq analysis.R

--- scATAC-seq/01_scATAC-seq analysis.R
+++ scATAC-seq/01_scATAC-seq analysis.R
@@ -89,6 +89,66 @@ projCELL1 <- ArchRProject(
   copyArrows = TRUE #Recommended so that if you modify Arrow files you have an original copy for later usage.
  )
 
+# Add doublet score. 
+doubScores <- addDoubletScores(
+  input = ArrowFiles, 
+  k = 10, # Refers to how many cells near a "pseudo-doublet" to count.
+  knnMethod = "UMAP", # Refers to the embedding to use for nearest neighbor search.
+  LSIMethod = 1,
+  force = TRUE
+  )
+
+# QC
+proj_CELL_1 <- projCELL1
+p <- ggPoint(
+  x = df[,1],
+  y = df[,2], 
+  colorDensity = TRUE,
+  continuousSet = "sambaNight", 
+  xlabel = "Log10UniqueFragments", 
+  ylabel = "TSS Enrichment", 
+  xlim = c(log10(500), quantile(df[,1], probs = 0.99)),
+  ylim = c(0, quantile(df[,2], probs = 0.99))
+  
+  ) + geom_hline(yintercept = 4, lty = "dashed") + geom_vline(xintercept = 3, lty = "dashed")
+plotPDF(p, name = "TSS-vs-Frags.pdf", ArchRProj = proj_CELL1, addDOC = FALSE)
+ 
+p1 <- plotGroups(
+    ArchRProj = proj_CELL_1, 
+    groupBy = "Sample", 
+    colorBy = "cellColData", 
+    name = "TSSEnrichment",
+    plotAs = "ridges"
+   )
+p2 <- plotGroups(
+    ArchRProj = proj_CELL_1, 
+    groupBy = "Sample", 
+    colorBy = "cellColData", 
+    name = "TSSEnrichment",
+    plotAs = "violin",
+    alpha = 0.4,
+    addBoxPlot = TRUE
+   )
+p3 <- plotGroups(
+    ArchRProj = proj_CELL_1, 
+    groupBy = "Sample", 
+    colorBy = "cellColData", 
+    name = "log10(nFrags)",
+    plotAs = "ridges"
+   )
+p4 <- plotGroups(
+    ArchRProj = proj_CELL_1, 
+    groupBy = "Sample", 
+    colorBy = "cellColData", 
+    name = "log10(nFrags)",
+    plotAs = "violin",
+    alpha = 0.4,
+    addBoxPlot = TRUE
+   )
+plotPDF(p1,p2,p3,p4, name = "QC-Sample-Statistics.pdf", ArchRProj = proj_CAD_1, addDOC = FALSE, width = 4, height = 4)
+
+
+
 # Session Information.
 Sys.Date()
 sessionInfo()
