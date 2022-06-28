eeb59c49cdd724d2b9e3229c87cc6808a7610eb4
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2022-06-28 11:08:24 -0500

    Update 01_scATAC_analysis_Signac.R

--- scATAC-seq/01_scATAC_analysis_Signac.R
+++ scATAC-seq/01_scATAC_analysis_Signac.R
@@ -5,8 +5,6 @@
 
 # module load R/4.1.1-gccmkl
 
-
-
 library(Signac)
 library(Seurat)
 library(SeuratDisk)
@@ -24,5 +22,33 @@ library(patchwork)
 library(stringr)
 library(repr)
 set.seed(1)
+
+# Read ATAC data.
+counts <- Read10X_h5("filtered_peak_bc_matrix_104.h5")
+
+# Read metadatal
+metadata <- read.csv(file = "singlecell_104.csv", 
+                     header = TRUE, 
+                     row.names = 1)
+# Create a chromatin assay utilizing the count matrix.
+brain_assay <- CreateChromatinAssay(counts = counts,
+                                    sep = c(":", "-"), 
+                                    genome = "mm10", 
+                                    fragments = "fragments_104.tsv.gz", 
+                                    min.cells = 1)
+# The ChromatinAssay class extends the standard Seurat Assay class and adds several additional slots for data useful for the analysis of single-cell chromatin datasets.
+# Create Seurat Object.
+brain <- CreateSeuratObject(counts = brain_assay, 
+                            assay = 'ATAC', 
+                            project = 'ATAC', 
+                            meta.data = metadata)
+                                  
+# Extract gene annotations from EnsDb. 
+annotations <- GetGRangesFromEnsDb(ensdb = EnsDb.Mmusculus.v79)
+seqlevelsStyle(annotations) <- 'UCSC'
+genome(annotations) <- "mm10"
+
+# Add the gene information to the object.
+Annotation(brain) <- annotations
 
 
